59243450e999e0a45e09edf4328735c0d9cc8e84
a01635018 <A01635018@itesm.mx>
2021-08-17 17:15:00 +0000

    Actividad enviada

--- assignments/00HelloWorld/src/exercise.py
+++ assignments/00HelloWorld/src/exercise.py
@@ -1,6 +1,8 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    print("Hello world")
+
+    
 
 if __name__=='__main__':
     main()
--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
+    num1 = int(input("Dame un número: "))
+    num2 = int(input("Dame un número: "))
+
+    Suma = num1 + num2
+    Resta = num1 - num2
+    Multi = num1 * num2
+
+    print("Suma:",Suma)
+    print("Resta:",Resta)
+    print("Multiplicación:",Multi)
+
 
 
 
